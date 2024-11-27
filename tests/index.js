d4e659d2063110ea173f88344678d5ff539cfe10
Jake Low <hello@jakelow.com>
2024-11-27 14:22:36 -0800

    Rename *.osc test files to *.adiff

--- tests/index.js
+++ tests/index.js
@@ -2,17 +2,17 @@ import fs from 'node:fs';
 import tape from 'tape';
 import parser from '../index.js';
 
-const isOSCFile = (filename) => (/^.*\.osc$/).test(filename);
+const isAdiffFile = (filename) => (/^.*\.adiff$/).test(filename);
 const stripExtension = (filename) => filename.split('.')[0];
 
 const filenames =
   fs.readdirSync('tests/data', { encoding: 'utf-8' })
-    .filter(isOSCFile)
+    .filter(isAdiffFile)
     .map(stripExtension);
 
 for (const filename of filenames) {
   tape(`testing file: ${filename}`, function(t) {
-    const xml = fs.readFileSync(`tests/data/${filename}.osc`, { encoding: 'utf-8' });
+    const xml = fs.readFileSync(`tests/data/${filename}.adiff`, { encoding: 'utf-8' });
     const expected = JSON.parse(fs.readFileSync(`tests/data/${filename}.json`));
 
     parser(xml).then((actual) => {
